55ecef296d89dd2207b5805252b39916e70f2a11
krupt <krupt25@gmail.com>
2015-10-15 10:41:17 +0500

    jdbc properties

--- src/main/java/ru/rti/controller/ApplicationController.java
+++ src/main/java/ru/rti/controller/ApplicationController.java
@@ -1,6 +1,5 @@
 package ru.rti.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,7 +12,7 @@ public class ApplicationController {
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseBody
 	public String getAppName() {
-		return "Hello, world!<br>Привет, мир!";
+		return "Hello, world!<br>Привет, мир!<br>Da";
 	}
 
 }
